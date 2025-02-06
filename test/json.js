029d95841406ea2b4b57becb3c055eeb0352e37e
numb2too <milk518abc@gmail.com>
2025-02-06 08:48:20 +0800

    不錯版本2

---
+++ test/json.js
@@ -0,0 +1,56 @@
+const myTestJson = [
+    {
+        id: 'sn11',
+        type: 'food',
+        name: '初來甜點スイーツ',
+        remember:'我會為了瑪德蓮特地跑來吃。榛果可可瑪德蓮真的是好吃到一次吃三顆。來白沙屯甜點唯一首選',
+        address: '357苗栗縣通霄鎮12鄰67-3號',
+        googleMapUrl: [
+          { title: 'googleMap', url: 'https://maps.app.goo.gl/SRSkoRBe6SJXa6xn9' }
+        ],
+        geoJson: {
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [
+                  120.7099826,
+                  24.5703745,
+                  0
+                ]
+              },
+              "properties": {
+                "name": "初來甜點スイーツ",
+                "styleUrl": "#__managed_style_33BC58E910360B93FF84",
+                "fill-opacity": 0.25098039215686274,
+                "fill": "#ffffff",
+                "stroke-opacity": 1,
+                "stroke": "#fbc02d",
+                "stroke-width": 2.28571,
+                "icon-offset": [
+                  64,
+                  128
+                ],
+                "icon-offset-units": [
+                  "pixels",
+                  "insetPixels"
+                ],
+                "icon": "https://earth.google.com/earth/document/icon?color=ffee58&id=2000&scale=4"
+              },
+              "id": "0E69CAB7F3360B93FF81"
+            }
+          ]
+        },
+        videos: [
+          'https://youtu.be/Kic6cLlAbLw?si=IyMlHj4j70M9vyfY',
+          'https://youtube.com/shorts/U6xkOqJ8ZIs?si=cy-8buN4-4CjifXY'
+        ],
+        level: 5,
+        instagram: ['https://www.instagram.com/p/C8rI0RgSaVA/','https://www.instagram.com/p/C8rI0RgSaVA/'],
+        img:'https://instagram.frmq2-2.fna.fbcdn.net/v/t51.2885-15/475580469_488193844039813_4900564716188244343_n.jpg?stp=dst-jpg_e15_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi42NDB4MTEzNi5zZHIuZjcxODc4LmRlZmF1bHRfY292ZXJfZnJhbWUifQ&_nc_ht=instagram.frmq2-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=1m4dxDJj3gcQ7kNvgE_pwqJ&_nc_gid=7e016ad6a7524f6a801e4854a7455fc0&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzU2MDQ4NzU0NDUwMjc2NjEzMw%3D%3D.3-ccb7-5&oh=00_AYCpr9sa99MRL6ms8RcWfwz1C4oZU3jHpB8YfmuGgvS0uA&oe=67A9CFAB&_nc_sid=7a9f4b',
+        attractions: ['瑪德蓮','甜點','蛋糕','白沙屯美食']
+      },
+]
+   
